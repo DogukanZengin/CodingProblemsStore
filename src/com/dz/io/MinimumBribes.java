20fa9f63d44b7473249e96d4ff3c6b610c472fe1
dogukanzengin <dogukanzen@gmail.com>
2019-12-14 15:23:58 +0300

    - minimum bribes in a queue

---
+++ src/com/dz/io/MinimumBribes.java
@@ -0,0 +1,55 @@
+package com.dz.io;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class MinimumBribes {
+
+    static void minimumBribes(int[] q) {
+
+            int count = 0 ;
+            int midOfThree = Integer.MAX_VALUE;
+            int maxOfThree =  Integer.MAX_VALUE;
+            int minOfThree =  Integer.MAX_VALUE ;
+
+            for(int i=q.length-1 ; i >= 0 ; i--){
+                //person has no way to move more than two positions -> wrong
+                if((q[i]-i) > 3 ) {
+                    System.out.println("Too chaotic");
+                    return;
+                }else{
+                    //means current value has passed at least 3 values -> wrong
+                    if(q[i] > maxOfThree){
+                        System.out.println("Too chaotic");
+                        return;
+                    }
+                    else if(q[i] > midOfThree){ //means -> current value has bribed 2 ppl
+                        count=count+2;
+                    }
+                    else if(q[i] > minOfThree){ //means-> current value has bribed 1 person.
+                        count=count+1;
+                    }
+
+                    //Now adjust minThree values comparing, taking the current vlaue to account
+                    if(q[i]<minOfThree){
+                        maxOfThree=midOfThree;
+                        midOfThree=minOfThree;
+                        minOfThree = q[i];
+                    }
+                    else if(q[i]<midOfThree){
+                        maxOfThree=midOfThree;
+                        midOfThree = q[i];
+                    }
+                    else if(q[i]<maxOfThree){
+                        maxOfThree = q[i];
+                    }
+                }
+            }
+            System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        minimumBribes(new int[]{2,5,1,3,4});
+    }
+}
