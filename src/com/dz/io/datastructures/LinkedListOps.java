f4c25b00132f30631a08313984a101e9e001f499
dogukanzengin <dogukanzen@gmail.com>
2019-12-18 16:40:49 +0300

    - Added reverse

--- src/com/dz/io/datastructures/LinkedListOps.java
+++ src/com/dz/io/datastructures/LinkedListOps.java
@@ -1,17 +1,28 @@
 package com.dz.io.datastructures;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.Set;
 
 public class LinkedListOps {
     private static class Node {
         int item;
         Node next;
+        Node prev;
 
         Node(int element,Node next) {
             this.item = element;
             this.next = next;
+        }
+
+        Node(int element,Node next, Node prev) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
         }
     }
     /**
@@ -93,5 +104,92 @@ public class LinkedListOps {
         previous.next = newCurrent;
 
         return head;
+    }
+
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        while(head.next != null){
+            DoublyLinkedListNode next = head.next;
+            head.next = head.prev;
+            head.prev = next;
+            head = next;
+        }
+        head.next = head.prev;
+        head.prev = null;
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            DoublyLinkedListNode llist1 = reverse(llist.head);
+
+            printDoublyLinkedList(llist1, " ");
+        }
+
+        scanner.close();
+    }
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
     }
 }
