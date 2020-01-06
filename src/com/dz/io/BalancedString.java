050f65f6c41dd303463cc9b6cdcdd097e6611291
dogukanzengin <dogukanzen@gmail.com>
2020-01-06 16:56:46 +0000

    - String anagram added

--- src/com/dz/io/BalancedString.java
+++ src/com/dz/io/BalancedString.java
@@ -1,5 +1,11 @@
 package com.dz.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Balanced strings are those who have equal quantity of 'L' and 'R' characters.
  *
@@ -9,11 +15,6 @@ package com.dz.io;
  */
 public class BalancedString {
 
-    public static void main(String[] args) {
-        System.out.println(balancedStringSplit("RLRRRLLRLL"));
-
-    }
-
     static int balancedStringSplit(String s) {
             int total = 0, count = 0;
             for(int i = 0;i<s.length();i++){
@@ -22,5 +23,35 @@ public class BalancedString {
             }
 
             return total;
+    }
+
+    // Complete the makeAnagram function below.
+    static int makeAnagram(String a, String b) {
+        List<Character> characters = b.chars().mapToObj(operand -> (char) operand).collect(Collectors.toList());
+        List<Character> extras = new ArrayList<>();
+        for(int i = 0;i<a.length();i++){
+            if(characters.contains(a.charAt(i))){
+                characters.remove(Character.valueOf(a.charAt(i)));
+            }else{
+                extras.add(a.charAt(i));
+            }
+        }
+        characters.addAll(extras);
+        return characters.size();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        int res = makeAnagram(a, b);
+
+        System.out.println(res);
+
+        scanner.close();
     }
 }
